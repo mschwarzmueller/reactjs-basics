bade0b00115203e5e66a3ae77677f2b435d5253a
Maximilian Schwarzmüller <mblacky0@gmail.com>
2016-07-25 15:27:37 +0200

    added props

--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -2,9 +2,11 @@ import React from "react";
 
 export class Home extends React.Component {
     render() {
+        const age = this.props.age + 1;
         return (
             <div>
-                <p>In a new Component!</p>
+                <h3>Hello {this.props.name}!</h3>
+                <p>You're almost {age}</p>
             </div>
         );
     }
--- src/app/index.js
+++ src/app/index.js
@@ -16,7 +16,7 @@ class App extends React.Component {
                 </div>
                 <div className="row">
                     <div className="col-xs-10 col-xs-offset-1">
-                        <Home/>
+                        <Home name={"Max"} age={27}/>
                     </div>
                 </div>
             </div>
