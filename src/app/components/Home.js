3109a6b7f9cf58f59ffd5db320ca8fe4599e8379
Maximilian Schwarzmüller <mblacky0@gmail.com>
2016-07-25 15:25:26 +0200

    added multiple components

---
+++ src/app/components/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export class Home extends React.Component {
+    render() {
+        return (
+            <div>
+                <p>In a new Component!</p>
+            </div>
+        );
+    }
+}
--- src/app/index.js
+++ src/app/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { render } from 'react-dom';
 
+import { Header } from "./components/Header";
+import { Home } from "./components/Home";
+
 class App extends React.Component {
 
     render() {
@@ -8,7 +11,12 @@ class App extends React.Component {
             <div className="container">
                 <div className="row">
                     <div className="col-xs-10 col-xs-offset-1">
-                        <h1>ReactJS Basics</h1>
+                        <Header/>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-xs-10 col-xs-offset-1">
+                        <Home/>
                     </div>
                 </div>
             </div>
