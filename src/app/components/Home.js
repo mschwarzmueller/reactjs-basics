4a5b6cf91223e7c3606f88b4099c2a6d51e0e6de
Maximilian Schwarzmüller <mblacky0@gmail.com>
2016-07-25 15:35:04 +0200

    handle events + events as props

--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -1,12 +1,24 @@
 import React from "react";
 
 export class Home extends React.Component {
+    constructor(props) {
+        super();
+        this.age = props.age;
+    }
+
+    onMakeOlder() {
+        this.age += 3; // doesn't update the UI
+    }
+
     render() {
-        const age = this.props.age + 1;
+        const age = this.age + 1;
         return (
             <div>
                 <h3>Hello {this.props.name}!</h3>
                 <p>You're almost {age}</p>
+                <button onClick={this.onMakeOlder.bind(this)} className="btn btn-primary">Make me older!</button>
+                <hr/>
+                <button onClick={this.props.greet} className="btn btn-success">Greet me!</button>
             </div>
         );
     }
--- src/app/index.js
+++ src/app/index.js
@@ -5,6 +5,14 @@ import { Header } from "./components/Header";
 import { Home } from "./components/Home";
 
 class App extends React.Component {
+    constructor() {
+        super();
+        this.age = 27;
+    }
+
+    onSayHello() {
+        alert("Hello!");
+    }
 
     render() {
         return (
@@ -16,7 +24,7 @@ class App extends React.Component {
                 </div>
                 <div className="row">
                     <div className="col-xs-10 col-xs-offset-1">
-                        <Home name={"Max"} age={27}/>
+                        <Home name={"Max"} age={this.age} greet={this.onSayHello}/>
                     </div>
                 </div>
             </div>
