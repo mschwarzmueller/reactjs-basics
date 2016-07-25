a7ccdcf4eef9b08efb7f173243bc01ee2cbd8ff7
Maximilian Schwarzmüller <mblacky0@gmail.com>
2016-07-25 15:06:27 +0200

    finished project master

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ var DIST_DIR = path.resolve(__dirname, 'dist');
 var SRC_DIR = path.resolve(__dirname, 'src');
 
 var config = {
-    entry: SRC_DIR + '/app/index.jsx',
+    entry: SRC_DIR + '/app/index.js',
     output: {
         path: DIST_DIR + '/app',
         filename: 'bundle.js',
@@ -14,7 +14,7 @@ var config = {
     module: {
         loaders: [
             {
-                test: /\.jsx?/,
+                test: /\.js?/,
                 include: SRC_DIR,
                 loader: 'babel-loader',
                 query: {
