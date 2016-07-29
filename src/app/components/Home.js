3a6526b40fd872e2bf3317ddc2ecbf0289e0c568
Maximilian Schwarzmüller <mblacky0@gmail.com>
2016-07-29 11:58:29 +0200

    finished events

--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -1,22 +1,23 @@
 import React from "react";
 
 export class Home extends React.Component {
+    constructor(props) {
+        super();
+        this.age = props.age;
+    }
+
+    onMakeOlder() {
+        this.age += 3;
+        console.log(this.age);
+    }
+
     render() {
-        var text = "Something!";
         return (
             <div>
                 <p>In a new Component!</p>
-                <p>{text}</p>
-                <p>Your name is {this.props.name}, your age is {this.props.age}</p>
-                <p>User Object => Name: {this.props.user.name}</p>
-                <div>
-                    <h4>Hobbies</h4>
-                    <ul>
-                        {this.props.user.hobbies.map((hobby, i) => <li key={i}>{hobby}</li>)}
-                    </ul>
-                </div>
+                <p>Your name is {this.props.name}, your age is {this.age}</p>
                 <hr/>
-                {this.props.children}
+                <button onClick={() => this.onMakeOlder()} className="btn btn-primary">Make me older!</button>
             </div>
         );
     }
@@ -24,7 +25,5 @@ export class Home extends React.Component {
 
 Home.propTypes = {
     name: React.PropTypes.string,
-    age: React.PropTypes.number,
-    user: React.PropTypes.object,
-    children: React.PropTypes.element.isRequired
+    age: React.PropTypes.number
 };
--- src/app/index.js
+++ src/app/index.js
@@ -6,10 +6,6 @@ import {Home} from "./components/Home";
 
 class App extends React.Component {
     render() {
-        var user = {
-            name: "Anna",
-            hobbies: ["Sports", "Reading"]
-        };
         return (
             <div className="container">
                 <div className="row">
@@ -19,9 +15,7 @@ class App extends React.Component {
                 </div>
                 <div className="row">
                     <div className="col-xs-10 col-xs-offset-1">
-                        <Home name={"Max"} age={27} user={user}>
-                            <p>This is a paragraph!</p>
-                        </Home>
+                        <Home name={"Max"} age={27} />
                     </div>
                 </div>
             </div>
