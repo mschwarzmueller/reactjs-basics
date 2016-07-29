81bd2d8e68def1cbc7f320cbe9cb926596fee404
Maximilian Schwarzmüller <mblacky0@gmail.com>
2016-07-29 12:29:13 +0200

    finished component interaction

--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -5,7 +5,8 @@ export class Home extends React.Component {
         super();
         this.state = {
             age: props.initialAge,
-            status: 0
+            status: 0,
+            homeLink: "Changed Link"
         };
         setTimeout(() => {
             this.setState({
@@ -20,6 +21,10 @@ export class Home extends React.Component {
         });
     }
 
+    onChangeLink() {
+        this.props.changeLink(this.state.homeLink);
+    }
+
     render() {
         return (
             <div>
@@ -28,6 +33,10 @@ export class Home extends React.Component {
                 <p>Status: {this.state.status}</p>
                 <hr/>
                 <button onClick={() => this.onMakeOlder()} className="btn btn-primary">Make me older!</button>
+                <hr/>
+                <button onClick={this.props.greet} className="btn btn-primary">Greet</button>
+                <hr/>
+                <button onClick={this.onChangeLink.bind(this)} className="btn btn-primary">Change Header Link</button>
             </div>
         );
     }
@@ -35,5 +44,6 @@ export class Home extends React.Component {
 
 Home.propTypes = {
     name: React.PropTypes.string,
-    initialAge: React.PropTypes.number
+    initialAge: React.PropTypes.number,
+    greet: React.PropTypes.func
 };
--- src/app/index.js
+++ src/app/index.js
@@ -5,17 +5,39 @@ import {Header} from "./components/Header";
 import {Home} from "./components/Home";
 
 class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+          homeLink: "Home"
+        };
+    }
+
+    onGreet() {
+        alert("Hello!");
+    }
+
+    onChangeLinkName(newName) {
+        this.setState({
+            homeLink: newName
+        });
+    }
+
     render() {
         return (
             <div className="container">
                 <div className="row">
                     <div className="col-xs-10 col-xs-offset-1">
-                        <Header homeLink="Home"/>
+                        <Header homeLink={this.state.homeLink}/>
                     </div>
                 </div>
                 <div className="row">
                     <div className="col-xs-10 col-xs-offset-1">
-                        <Home name={"Max"} initialAge={27} />
+                        <Home
+                            name={"Max"}
+                            initialAge={27}
+                            greet={this.onGreet}
+                            changeLink={this.onChangeLinkName.bind(this)}
+                        />
                     </div>
                 </div>
             </div>
