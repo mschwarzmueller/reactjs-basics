202b57328e1ba7e8f144f3f3079c705805b3017d
Maximilian Schwarzmüller <mblacky0@gmail.com>
2016-07-29 12:39:57 +0200

    finished component interaction

--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -6,7 +6,7 @@ export class Home extends React.Component {
         this.state = {
             age: props.initialAge,
             status: 0,
-            homeLink: "Changed Link"
+            homeLink: props.initialLinkName
         };
         setTimeout(() => {
             this.setState({
@@ -25,6 +25,12 @@ export class Home extends React.Component {
         this.props.changeLink(this.state.homeLink);
     }
 
+    onHandleChange(event) {
+        this.setState({
+            homeLink: event.target.value
+        });
+    }
+
     render() {
         return (
             <div>
@@ -36,6 +42,8 @@ export class Home extends React.Component {
                 <hr/>
                 <button onClick={this.props.greet} className="btn btn-primary">Greet</button>
                 <hr/>
+                <input type="text" value={this.state.homeLink}
+                       onChange={(event) => this.onHandleChange(event)} />
                 <button onClick={this.onChangeLink.bind(this)} className="btn btn-primary">Change Header Link</button>
             </div>
         );
@@ -45,5 +53,6 @@ export class Home extends React.Component {
 Home.propTypes = {
     name: React.PropTypes.string,
     initialAge: React.PropTypes.number,
-    greet: React.PropTypes.func
+    greet: React.PropTypes.func,
+    initialLinkName: React.PropTypes.string
 };
--- src/app/index.js
+++ src/app/index.js
@@ -37,6 +37,7 @@ class App extends React.Component {
                             initialAge={27}
                             greet={this.onGreet}
                             changeLink={this.onChangeLinkName.bind(this)}
+                            initialLinkName={this.state.homeLink}
                         />
                     </div>
                 </div>
