38fb11aa92a2d264efc19402ae0cdaf207241fce
Maximilian Schwarzmüller <mblacky0@gmail.com>
2016-07-29 12:15:01 +0200

    stateless components

--- src/app/index.js
+++ src/app/index.js
@@ -10,7 +10,7 @@ class App extends React.Component {
             <div className="container">
                 <div className="row">
                     <div className="col-xs-10 col-xs-offset-1">
-                        <Header/>
+                        <Header homeLink="Home"/>
                     </div>
                 </div>
                 <div className="row">
