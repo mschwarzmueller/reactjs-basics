175df4f6da2b33f4b176b67773940d56584e9f67
Maximilian Schwarzmüller <mblacky0@gmail.com>
2016-07-29 11:48:48 +0200

    finished props

--- src/app/index.js
+++ src/app/index.js
@@ -1,11 +1,15 @@
 import React from "react";
-import { render } from "react-dom";
+import {render} from "react-dom";
 
-import { Header } from "./components/Header";
-import { Home } from "./components/Home";
+import {Header} from "./components/Header";
+import {Home} from "./components/Home";
 
 class App extends React.Component {
     render() {
+        var user = {
+            name: "Anna",
+            hobbies: ["Sports", "Reading"]
+        };
         return (
             <div className="container">
                 <div className="row">
@@ -15,7 +19,9 @@ class App extends React.Component {
                 </div>
                 <div className="row">
                     <div className="col-xs-10 col-xs-offset-1">
-                        <Home/>
+                        <Home name={"Max"} age={27} user={user}>
+                            <p>This is a paragraph!</p>
+                        </Home>
                     </div>
                 </div>
             </div>
