dd0de75d494832147156d18c4c854570452b7a54
Maximilian Schwarzmüller <mblacky0@gmail.com>
2016-07-29 12:09:56 +0200

    finished state and how the ui updates

--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -3,19 +3,29 @@ import React from "react";
 export class Home extends React.Component {
     constructor(props) {
         super();
-        this.age = props.age;
+        this.state = {
+            age: props.initialAge,
+            status: 0
+        };
+        setTimeout(() => {
+            this.setState({
+                status: 1
+            });
+        },3000);
     }
 
     onMakeOlder() {
-        this.age += 3;
-        console.log(this.age);
+        this.setState({
+            age: this.state.age + 3
+        });
     }
 
     render() {
         return (
             <div>
                 <p>In a new Component!</p>
-                <p>Your name is {this.props.name}, your age is {this.age}</p>
+                <p>Your name is {this.props.name}, your age is {this.state.age}</p>
+                <p>Status: {this.state.status}</p>
                 <hr/>
                 <button onClick={() => this.onMakeOlder()} className="btn btn-primary">Make me older!</button>
             </div>
@@ -25,5 +35,5 @@ export class Home extends React.Component {
 
 Home.propTypes = {
     name: React.PropTypes.string,
-    age: React.PropTypes.number
+    initialAge: React.PropTypes.number
 };
--- src/app/index.js
+++ src/app/index.js
@@ -15,7 +15,7 @@ class App extends React.Component {
                 </div>
                 <div className="row">
                     <div className="col-xs-10 col-xs-offset-1">
-                        <Home name={"Max"} age={27} />
+                        <Home name={"Max"} initialAge={27} />
                     </div>
                 </div>
             </div>
