9fa54d2bf6730285558cc8e2986c8832653f41b2
Maximilian Schwarzmüller <mblacky0@gmail.com>
2016-08-01 10:40:16 +0200

    finished routing

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,24 @@
-console.log("It works!");
+import React from "react";
+import {render} from "react-dom";
+import {Router, Route, browserHistory, IndexRoute} from "react-router";
+
+import {Root} from "./components/Root";
+import {Home} from "./components/Home";
+import {User} from "./components/User";
+
+class App extends React.Component {
+    render() {
+        return (
+            <Router history={browserHistory}>
+                <Route path={"/"} component={Root} >
+                    <IndexRoute component={Home} />
+                    <Route path={"user/:id"} component={User} />
+                    <Route path={"home"} component={Home} />
+                </Route>
+                <Route path={"home-single"} component={Home}/>
+            </Router>
+        );
+    }
+}
+
+render(<App />, window.document.getElementById('app'));
