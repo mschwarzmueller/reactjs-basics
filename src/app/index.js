17eb20df2e4bbaf9931f7ca8ed355d6c12dda7be
primashah01 <44518482+primashah01@users.noreply.github.com>
2018-10-27 21:40:34 +0530

    try to basic component in react

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function APP() {
+  return <h1>Hello, guys</h1>;
+}
 console.log("It works!");
+
+ReactDOM.render(<App />, document.getElementById('root'));
