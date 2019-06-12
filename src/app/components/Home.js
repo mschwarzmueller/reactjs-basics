b0dd0be46740ae0c889faeedcf38594ed127235a
Sam Apter <sam@sapter.com>
2019-06-12 12:53:25 -0400

    fix/update-propTypes

--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -1,39 +1,44 @@
 import React from "react";
+import { PropTypes } from "prop-types";
 
 export class Home extends React.Component {
-    constructor(props) {
-        super();
-        this.state = {
-            age: props.initialAge,
-            status: 0
-        };
-        setTimeout(() => {
-            this.setState({
-                status: 1
-            });
-        },3000);
-    }
+  constructor(props) {
+    super();
+    this.state = {
+      age: props.initialAge,
+      status: 0,
+    };
+    setTimeout(() => {
+      this.setState({
+        status: 1,
+      });
+    }, 3000);
+  }
 
-    onMakeOlder() {
-        this.setState({
-            age: this.state.age + 3
-        });
-    }
+  onMakeOlder() {
+    this.setState({
+      age: this.state.age + 3,
+    });
+  }
 
-    render() {
-        return (
-            <div>
-                <p>In a new Component!</p>
-                <p>Your name is {this.props.name}, your age is {this.state.age}</p>
-                <p>Status: {this.state.status}</p>
-                <hr/>
-                <button onClick={() => this.onMakeOlder()} className="btn btn-primary">Make me older!</button>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <p>In a new Component!</p>
+        <p>
+          Your name is {this.props.name}, your age is {this.state.age}
+        </p>
+        <p>Status: {this.state.status}</p>
+        <hr />
+        <button onClick={() => this.onMakeOlder()} className="btn btn-primary">
+          Make me older!
+        </button>
+      </div>
+    );
+  }
 }
 
 Home.propTypes = {
-    name: React.PropTypes.string,
-    initialAge: React.PropTypes.number
+  name: PropTypes.string,
+  initialAge: PropTypes.number,
 };
