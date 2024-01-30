43eb02b6b16e7ae3d91411704f91af6facb49bc4
Netaly <25016486+21shield@users.noreply.github.com>
2024-01-30 11:36:23 -0500

    creates initial component

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,12 @@
-console.log("It works!");
+import React from "react";
+import { render } from "react-dom";
+
+class App extends React.component {
+  render() {
+    return (
+      <div>
+        <h1> Hellow!</h1>
+      </div>
+    );
+  }
+}
